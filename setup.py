91620073bd4e76d0ae9d6ad8cb6e79b3f527e22b
pritishbodke <bodkepritish99@gmail.com>
2023-11-13 19:53:32 +0100

    Add setup.py for pip install

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='math_quiz',
+    name='math-quiz',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        # Add any dependencies your project may have
+    ],
     entry_points={
         'console_scripts': [
-            'math_quiz=math_quiz.math_quiz:main',
+            'math_quiz=math_quiz.math_quiz:math_quiz',
         ],
     },
 )
