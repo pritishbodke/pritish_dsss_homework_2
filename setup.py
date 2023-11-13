2a9b05835a11842f4c5245c8495f027001738ce6
pritishbodke <133962719+pritishbodke@users.noreply.github.com>
2023-11-13 19:08:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
     name='math_quiz',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:main',
+        ],
+    },
 )
